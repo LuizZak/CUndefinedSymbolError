6a924127f1c20243dd479b4b983c8dc3b3132d7f
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2025-11-29 13:39:56 -0300

    Reducing the reproduction further

--- Dependencies/SwiftCLib/Sources/CLibrary/include/clibrary/header.h
+++ Dependencies/SwiftCLib/Sources/CLibrary/include/clibrary/header.h
@@ -1,6 +1,5 @@
 #pragma once
 
 #include "symbol.h"
-#include "math_functions.h"
 
 void cCall(cSymbol symbol);
--- Dependencies/SwiftCLib/Sources/CLibrary/include/clibrary/symbol.h
+++ Dependencies/SwiftCLib/Sources/CLibrary/include/clibrary/symbol.h
@@ -1,30 +1,7 @@
 #pragma once
 
-#include <float.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stdint.h>
-
 typedef struct cSymbol {
     int field0;
 } cSymbol;
 
-typedef struct cSymbol2 {
-    cSymbol field0;
-} cSymbol2;
-
 static const cSymbol cSymbol_symbol = { 0 };
-
-static inline cSymbol cSumSymbols(cSymbol a, cSymbol b) {
-    return a;
-}
-
-#ifdef __cplusplus
-
-/// Unary add one vector to another
-inline cSymbol operator+(cSymbol a, cSymbol b)
-{
-	return cSumSymbols(a, b);
-}
-
-#endif
